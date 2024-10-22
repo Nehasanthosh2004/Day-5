862311e8d1d911d02675342dd10a19e5d2730894
neha santhosh <nehasanthosh132004@gmail.com>
2024-10-22 20:54:53 +0530

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { SlicePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ApiService } from '../../api.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink,SlicePipe],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(private api:ApiService) { }
+  products:any=[]
+
+  ngOnInit(){
+    this.api.getData().subscribe( (data:any)=>  {
+      this.products= data;
+    })
+    }
+
+}
